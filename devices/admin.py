a6149484ac8c0ccb45223ce007c9d7d652ac023e
tim <tim@tim-virtual-machine.(none)>
2012-12-08 15:04:19 -0800

    auto commit

--- devices/admin.py
+++ devices/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from models import Manufacture, Device, Configuration, Connection
+from models import Manufacture, Device, Configuration, Connection, UsersCode, CodeStatus, CodeExample, CmdStatus, SerialMonitor, ExecutedCode
 
 admin.site.register(Manufacture)
 admin.site.register(Device)
 admin.site.register(Configuration)
 admin.site.register(Connection)
+admin.site.register(UsersCode)
+admin.site.register(CodeStatus)
+admin.site.register(CodeExample)
+admin.site.register(CmdStatus)
+admin.site.register(SerialMonitor)
+admin.site.register(ExecutedCode)
