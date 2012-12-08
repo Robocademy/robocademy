0b22833da72beadddd17a59f2b8aebe5267f23e9
tim <tim@tim-virtual-machine.(none)>
2012-12-08 00:47:03 -0800

    auto commit

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,2 @@
 from django.contrib import admin
-from models import DeviceConfiguration, Lesson, ExecutedCode, CommandPattern
-
-admin.site.register(DeviceConfiguration)
-admin.site.register(Lesson)
-admin.site.register(ExecutedCode)
-admin.site.register(CommandPattern)
+# from models import
