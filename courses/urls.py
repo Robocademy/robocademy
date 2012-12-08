80210737375baef088450d00ade408a306e5252a
tim <tim@tim-virtual-machine.(none)>
2012-12-08 14:56:13 -0800

    auto commit

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.simple import direct_to_template
 
 
 urlpatterns = patterns('courses.views',
-    url(r'^arduino/$', 'arduino'),
+    #url(r'^arduino/$', 'arduino'),
     url(r'^arduino_expert/$', 'arduino_expert'),
 
 )
