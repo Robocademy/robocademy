2adbc3ba7240f66e144e137900bd37e4708551a1
tim <tim@tim-virtual-machine.(none)>
2012-12-08 15:59:34 -0800

    auto commit

--- devices/views.py
+++ devices/views.py
@@ -94,7 +94,8 @@ def set_serial_monitor(request):
         
 def get_examples(request, connection_id):
     connection = Connection.objects.get(id=connection_id)
-    return CodeExamples.objects.filter(connection=connection)
+    response_data = CodeExamples.objects.filter(connection=connection)
+    return HttpResponse(json.dumps(response_data), mimetype="application/json")
     
 def set_status(request, connection_id):
     connection = Connection.objects.get(id=connection_id)
