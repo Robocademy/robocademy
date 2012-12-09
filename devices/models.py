d12a7a22121a03dc7d1c902b6adabf9403cdb81e
tim <tim@tim-virtual-machine.(none)>
2012-12-08 16:07:17 -0800

    auto commit

--- devices/models.py
+++ devices/models.py
@@ -99,6 +99,26 @@ class CodeExample(models.Model):
     datetime = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200, unique=True)
     code = models.TextField()
+    
+    def get_dict(self, key_format=None):
+        "Returns a dictionary containing field names and values for the given instance"
+        instance = self
+        from django.db.models.fields.related import ForeignKey
+        if key_format:
+            assert '%s' in key_format, 'key_format must contain a %s'
+        key = lambda key: key_format and key_format % key or key
+
+        d = {}
+        for field in instance._meta.fields:
+            attr = field.name
+            value = getattr(instance, attr)
+            if value is not None and isinstance(field, ForeignKey):
+                value = value._get_pk_val()
+            d[key(attr)] = value
+        for field in instance._meta.many_to_many:
+            d[key(field.name)] = [obj._get_pk_val() for obj in getattr(instance, field.attname).all()]
+        d.update({'stream': self.get_connection()})
+        return d
 
 class CmdStatus(models.Model):
     connection = models.ForeignKey(Connection)   
