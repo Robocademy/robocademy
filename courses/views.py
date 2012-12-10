5e46841821717c93864b1b6303cb4569014691c2
tim <tim@tim-virtual-machine.(none)>
2012-12-10 07:19:10 -0800

    auto commit

--- courses/views.py
+++ courses/views.py
@@ -9,5 +9,5 @@ def arduino_expert(request):
     #context = RequestContext(request, {'lessons': Lesson.objects.filter(device_configuration__id=1).order_by('order')})
     #context.update({'examples': Code.objects.all(), 'default_example': Lesson.objects.get(device_configuration__id=1, order=1).start_code})
     #context.update({'examples': Code.objects.all(), 'default_example': Code.objects.get(title='Hello World').code})
-    return render_to_response('courses/arduino_expert.html', {})    
+    return render_to_response('courses/arduino_expert.html', RequestContext(request, {}))    
     
