66a49c2928891152328a22d2d4d371d77d7ad2a1
tim <tim@tim-virtual-machine.(none)>
2012-12-11 14:48:47 -0800

    auto commit

--- interactive_courses/views.py
+++ interactive_courses/views.py
@@ -8,3 +8,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 def index(request):
     context = RequestContext(request, {'courses': Course.objects.all()}) 
     return render_to_response('interactive_courses/index.html', context)
+    
+def course(request, slug):
+    context = RequestContext(request, {'course': Course.objects.get(slug=slug)})
+    return render_to_response('interactive_courses/course_embed.html', context)
