e83a8009efc30c1a47de497f2273574a40f15d3c
tim <tim@tim-virtual-machine.(none)>
2012-12-11 15:25:36 -0800

    auto commit

--- static/js/course.js
+++ static/js/course.js
@@ -1,18 +1,18 @@
-jQuery("#youtube-player-container").tubeplayer({
-	width: 600, // the width of the player
-	height: 450, // the height of the player
-	allowFullScreen: "true", // true by default, allow user to go full screen
-	initialVideo: "NnVT1E1trfM", // the video that is loaded into the player
-	preferredQuality: "default",// preferred quality: default, small, medium, large, hd720
-	onPlay: function(id){}, // after the play method is called
-	onPause: function(){}, // after the pause method is called
-	onStop: function(){$("#youtube-player-container").html('done');}, // after the player is stopped
-	onSeek: function(time){}, // after the video has been seeked to a defined point
-	onMute: function(){}, // after the player is muted
-	onUnMute: function(){} // after the player is unmuted
-});
+
 	
 
 $(function() {
-
+    $("#youtube-player-container").tubeplayer({
+        width: 600, // the width of the player
+        height: 450, // the height of the player
+        allowFullScreen: "true", // true by default, allow user to go full screen
+        initialVideo: "NnVT1E1trfM", // the video that is loaded into the player
+        preferredQuality: "default",// preferred quality: default, small, medium, large, hd720
+        onPlay: function(id){}, // after the play method is called
+        onPause: function(){}, // after the pause method is called
+        onStop: function(){$("#youtube-player-container").html('done');}, // after the player is stopped
+        onSeek: function(time){}, // after the video has been seeked to a defined point
+        onMute: function(){}, // after the player is muted
+        onUnMute: function(){} // after the player is unmuted
+    });
 });
