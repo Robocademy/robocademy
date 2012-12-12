01a6ef65ae5a414e29b1623e1463908533023a8e
tim <tim@tim-virtual-machine.(none)>
2012-12-12 05:41:38 -0800

    auto commit

--- static/js/course.js
+++ static/js/course.js
@@ -65,7 +65,7 @@ function getData()
         {
             alert('blah');
             lesson = lessons[lesson_order - 1];
-            $(this).parent().parent().tubeplayer({
+            $(this).parent().tubeplayer({
                 autoPlay: true,
                 width: 600, // the width of the player
                 height: 450, // the height of the player
