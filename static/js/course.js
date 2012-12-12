8073add9754a5a7ca6e3fb74149b42a994999a1d
tim <tim@tim-virtual-machine.(none)>
2012-12-11 17:33:05 -0800

    auto commit

--- static/js/course.js
+++ static/js/course.js
@@ -1,18 +1,34 @@
-
-	
+function getData()
+{
+    $.ajax({
+      url: 'get_course_data/',
+      type: "GET",
+      success: function(response) {
+        var lessons = response.lessons;
+        for (var i = 0; i < lessons.length; i++)
+        {
+            var lesson = lessons[i];
+            $("body").append("<div id='video_"+lesson.order+"'></div>");
+            $("#video_"+lesson.order).tubeplayer({
+                width: 600, // the width of the player
+                height: 450, // the height of the player
+                allowFullScreen: "true", // true by default, allow user to go full screen
+                initialVideo: "NnVT1E1trfM", // the video that is loaded into the player
+                preferredQuality: "default",// preferred quality: default, small, medium, large, hd720
+                onPlay: function(id){}, // after the play method is called
+                onPause: function(){}, // after the pause method is called
+                onPlayerEnded: function(){$("#youtube-player-container").html('What is the parts');}, // after the player is stopped
+                onSeek: function(time){}, // after the video has been seeked to a defined point
+                onMute: function(){}, // after the player is muted
+                onUnMute: function(){} // after the player is unmuted
+            });
+        }
+      }
+    });
+}	
 
 $(function() {
-    $("#youtube-player-container").tubeplayer({
-        width: 600, // the width of the player
-        height: 450, // the height of the player
-        allowFullScreen: "true", // true by default, allow user to go full screen
-        initialVideo: "NnVT1E1trfM", // the video that is loaded into the player
-        preferredQuality: "default",// preferred quality: default, small, medium, large, hd720
-        onPlay: function(id){}, // after the play method is called
-        onPause: function(){}, // after the pause method is called
-        onPlayerEnded: function(){$("#youtube-player-container").html('What is the parts');}, // after the player is stopped
-        onSeek: function(time){}, // after the video has been seeked to a defined point
-        onMute: function(){}, // after the player is muted
-        onUnMute: function(){} // after the player is unmuted
-    });
+    getData()
+    
+
 });
