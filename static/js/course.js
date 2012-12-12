f7cb8311ffa79432456a567d186f60e4dbaebcae
tim <tim@tim-virtual-machine.(none)>
2012-12-11 17:40:24 -0800

    auto commit

--- static/js/course.js
+++ static/js/course.js
@@ -9,11 +9,12 @@ function getData()
         {
             var lesson = lessons[i];
             $("body").append("<div id='video_"+lesson.order+"'></div>");
+            
             $("#video_"+lesson.order).tubeplayer({
                 width: 600, // the width of the player
                 height: 450, // the height of the player
                 allowFullScreen: "true", // true by default, allow user to go full screen
-                initialVideo: "NnVT1E1trfM", // the video that is loaded into the player
+                initialVideo: lesson.video_id, // the video that is loaded into the player
                 preferredQuality: "default",// preferred quality: default, small, medium, large, hd720
                 onPlay: function(id){}, // after the play method is called
                 onPause: function(){}, // after the pause method is called
