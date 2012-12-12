fc2893cbe89ab0a2e8081a75d7cf2adac845a344
tim <tim@tim-virtual-machine.(none)>
2012-12-12 14:37:18 -0800

    auto commit

--- static/js/course.js
+++ static/js/course.js
@@ -60,6 +60,7 @@ function getData()
     });
     $('.checkbox_answer').live('click', function() {
         console.log(answer_ids);
+        console.log($(this).val());
         console.log(jQuery.inArray($(this).val(), answer_ids));
         if (jQuery.inArray($(this).val(), answer_ids) == -1)
         {
