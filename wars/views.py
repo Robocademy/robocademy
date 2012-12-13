d18c47feca79edfe4c8374f9a73d62f26ec148fa
tim <tim@tim-virtual-machine.(none)>
2012-12-13 09:08:53 -0800

    auto commit

--- wars/views.py
+++ wars/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext
+import json
+from django.shortcuts import render_to_response
+from django.contrib.admin.views.decorators import staff_member_required
+
+def get_war_info(request):
+    return render_to_response('', {})
