65c3883e3022cc11d4de6be016bff898b5300d74
tim <tim@tim-virtual-machine.(none)>
2012-12-13 09:57:27 -0800

    auto commit

--- interactive_courses/views.py
+++ interactive_courses/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 import json
-from models import Course, Video
+from models import Course, Video, QuestionAndAnswer
 from django.shortcuts import render_to_response
 from django.contrib.admin.views.decorators import staff_member_required
 
