19a715bb23681279afd552a7f10ac86b31c21590
tim <tim@tim-virtual-machine.(none)>
2012-12-13 09:59:33 -0800

    auto commit

--- interactive_courses/views.py
+++ interactive_courses/views.py
@@ -37,7 +37,7 @@ def admin_save(request, slug):
         video = Video.objects.filter(lesson=lesson)[0]
         video.url = request.POST['lesson_%s_video_id' % (lesson.order)]
         video.save()
-        question = QuestionAndAnswer.objects.filter(lesson=self)[0]
+        question = QuestionAndAnswer.objects.filter(lesson=lesson)[0]
         question.statement = request.POST['lesson_%s_question' % (lesson.order)]
         question.save()
         lesson.save()
