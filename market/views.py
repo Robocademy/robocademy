186bdf9a9130c915f929e3d0f1434a2a858f22ce
tim <tim@tim-virtual-machine.(none)>
2012-12-17 17:52:59 -0800

    auto commit

--- market/views.py
+++ market/views.py
@@ -7,5 +7,5 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.contenttypes.models import ContentType
 
 def index(request):
-    context = RequestContext(request, {'categories': Category.objects.all()}) 
+    context = RequestContext(request, {'categories': [i.category for i in CategoryOrder.objects.all().order_by('order')]}) 
     return render_to_response('market/index.html', context)
