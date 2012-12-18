28a80d1d0f0a538c64c8bc8cafef7039742bf45d
tim <tim@tim-virtual-machine.(none)>
2012-12-17 16:49:18 -0800

    auto commit

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from models import Idea, Enity
+from robocademy.market.models import Category, Idea, Enity
 
-#admin.register(Category)
+admin.register(Category)
 admin.register(Idea)
 admin.register(Entity)
