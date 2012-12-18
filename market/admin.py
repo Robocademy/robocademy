72b5f56364741e55c22a84220f36166f80e92184
tim <tim@tim-virtual-machine.(none)>
2012-12-17 18:09:21 -0800

    auto commit

--- market/admin.py
+++ market/admin.py
@@ -4,4 +4,4 @@ from models import Category, Idea, Entity, CategoryOrder
 admin.site.register(Category)
 admin.site.register(Idea)
 admin.site.register(Entity)
-admin.site.register(CategoryAdmin)
+admin.site.register(CategoryOrder)
