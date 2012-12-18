714de0309d14fd24cc05b3d6fce3013df2dc2ce9
tim <tim@tim-virtual-machine.(none)>
2012-12-17 17:06:53 -0800

    auto commit

--- market/models.py
+++ market/models.py
@@ -3,9 +3,18 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     
+    def __unicode__(self):
+        return self.name
+    
 class Idea(models.Model):
     summary = models.CharField(max_length=50)
     description = models.TextField()
+    
+    def __unicode__(self):
+        return self.summary
 
 class Entity(models.Model):
     name = models.CharField(max_length=50)
+    
+     def __unicode__(self):
+        return self.name
