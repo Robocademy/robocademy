06cf96edebc562ddea53074a3f0b2acb10b3ff62
timothyclemans <timothy.clemans@gmail.com>
2012-12-18 17:26:39 -0800

    auto commit

--- interactive_courses/views.py
+++ interactive_courses/views.py
@@ -49,7 +49,7 @@ def admin_save(request, slug):
             checkbox_question = CheckboxQuestion2()
             checkbox_answer = CheckboxAnswer2()
             for h, cq in enumerate(sorted([i for i in request.POST.keys() if re.match('\w+_%s_question_%s_answer_choice_\d+' % (lesson_order, question_order), i)])):
-                checkbox = Checkbox2(order=h, value=request.POST['cq'])
+                checkbox = Checkbox2(order=h, value=request.POST[cq])
                 checkbox.save()
                 checkbox_question.checkboxes.add(checkbox)
                 if request.POST[cq.replace('answer_choice', 'answer')] == 'true':
