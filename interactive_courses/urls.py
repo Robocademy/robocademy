b90629b1172bbb4ae6929991ec3c38d91061564b
timothyclemans <timothy.clemans@gmail.com>
2012-12-19 20:44:13 -0800

    auto commit

--- interactive_courses/urls.py
+++ interactive_courses/urls.py
@@ -4,10 +4,11 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('interactive_courses.views',
     url(r'^$', 'index'),
+    url(r'^create_course/$', 'create_course'),
     url(r'^(?P<slug>[\w_]+)/$', 'course'),
     url(r'^(?P<slug>[\w_]+)/get_course_data/$', 'get_course_data'),
     url(r'^(?P<slug>[\w_]+)/admin/$', 'course_admin'),
     url(r'^(?P<slug>[\w_]+)/admin/save/$', 'admin_save'),
     url(r'^(?P<slug>[\w_]+)/embed/$', 'course_embed'),
-    url(r'^create_course/$', 'create_course'),
+
 )
