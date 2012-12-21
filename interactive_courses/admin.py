d4fbfbb295ad21c578f47b6ef1f0531437853e15
timothyclemans <timothy.clemans@gmail.com>
2012-12-21 13:32:54 -0800

    auto commit

--- interactive_courses/admin.py
+++ interactive_courses/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from models import Course, CourseAuthorRelationship, Lesson, QuestionAndAnswer, CheckboxQuestion2, CheckboxAnswer2, Checkbox2, Video
+from models import Course, CourseAuthorRelationship, Lesson, LessonContent, QuestionAndAnswer, CheckboxQuestion2, CheckboxAnswer2, Checkbox2, Video
 
 admin.site.register(Course)
 admin.site.register(CourseAuthorRelationship)
 admin.site.register(Lesson)
+admin.site.register(LessonContent)
 admin.site.register(QuestionAndAnswer)
 admin.site.register(CheckboxQuestion2)
 admin.site.register(CheckboxAnswer2)
