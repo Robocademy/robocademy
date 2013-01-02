f9d87354d49d77218f00122aac86d0c1c6ee6fb4
timothyclemans <timothy.clemans@gmail.com>
2013-01-02 01:12:37 -0800

    auto commit

--- fix_is_serial_ok.py
+++ fix_is_serial_ok.py
@@ -1,3 +1,6 @@
-f = open('is_serial_ok.txt', 'w')
-f.write('true')
-f.close()
+while True:
+    import time
+    time.sleep(5 * 60)
+    f = open('is_serial_ok.txt', 'w')
+    f.write('true')
+    f.close()
